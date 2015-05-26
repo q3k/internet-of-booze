922bfd0a78753e26f343550c07ae0c5c56062130
Sergiusz 'q3k' Bazański <q3k@q3k.org>
2015-05-26 15:07:35 +0200

    Reorganize stuff around.

--- task/software/generate-message.py
+++ task/software/generate-message.py
@@ -2,17 +2,16 @@ import sys
 
 secret = '93lf4s'
 magic = 'c29j'
-
 out = ""
-
 buf = ""
-# 08000df4
-buf += '\xf5\x0d\x00\x08' * 18
-#buf += chr(0)
+
+# Exploit (jump to xVend)
+#buf += '\xe9\x0d\x00\x08' * 18
+# Communicate, send 0th command
+buf += chr(0)
+
 for i, c in enumerate(magic + buf):
     o = (ord(c) ^ ((ord(secret[i%6]) +i)&0xFF))
     out = out + chr(o)
 
 print 'CTRL' + out.encode('hex')
-
-print len('CTRL' + out.encode('hex'))
